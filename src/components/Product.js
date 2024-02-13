a50db22f05979cb4b23b838d818e6d46071ede5d
Shovickbarua <shovickbarua92@gmail.com>
2024-02-13 23:43:49 +0600

    redux shopping cart

---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Product.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store/card-slice";
+import { useSelector } from "react-redux";
+const Product = ({ name, id, imgURL, price }) => {
+  const cartItems = useSelector((state) => state.cart.itemList);
+  const dispatch = useDispatch();
+  const addToCart = () => {
+    dispatch(cartActions.addToCart({
+      name, id, price, 
+    }))
+  }
+  return (
+    <div className="card">
+      <img src={imgURL} alt={name} />
+      <h2>{name}</h2>
+      <p>$ {price}</p>
+      <button onClick={() => addToCart()}>Add to cart</button>
+    </div>
+  );
+};
+
+export default Product;
